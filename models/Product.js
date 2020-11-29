d0eeb80bcd3ad929e092fd27d647e69944bf94d7
sujanpoojary <sujanpoojary884@gmail.com>
2020-11-29 10:46:41 +0530

    initial design

---
+++ models/Product.js
@@ -0,0 +1,10 @@
+module.exports = class Product {
+  constructor(id, title, price, imageUrl, description) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
+  save() {}
+};
